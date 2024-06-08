47a69bb777b48d7fb46fe92b084c94f3699e2edf
Taha Tesser <tessertaha@gmail.com>
2024-06-08 21:22:14 +0300

    Update basic sample

--- lib/basic.dart
+++ lib/basic.dart
@@ -19,22 +19,62 @@ class MyApp extends StatelessWidget {
                 WoltModalSheet.show<void>(
                   context: context,
                   pageListBuilder: (modalSheetContext) {
-                    return List<SliverWoltModalSheetPage>.generate(
-                      3,
-                      (index) => WoltModalSheetPage(
+                    final Widget stickyActionBar = Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FilledButton(
+                          onPressed: Navigator.of(modalSheetContext).pop,
+                          child: const Text('Close'),
+                        ),
+                        FilledButton(
+                          onPressed:
+                              WoltModalSheet.of(modalSheetContext).showNext,
+                          child: const Text('Next'),
+                        ),
+                      ],
+                    );
+                    final List<WoltModalSheetPage> pages = <WoltModalSheetPage>[
+                      WoltModalSheetPage(
+                        stickyActionBar: stickyActionBar,
                         child: Column(
                           children: <Widget>[
                             Image.network('https://i.imgur.com/3uuQ0Y0.jpeg'),
-                            FilledButton(
-                              onPressed: Navigator.of(modalSheetContext).pop,
-                              child: const Text('Close Sheet'),
-                            ),
+                            const Placeholder(fallbackHeight: 1000),
                           ],
                         ),
                       ),
-                    );
+                      WoltModalSheetPage(
+                        stickyActionBar: stickyActionBar,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Page 2',
+                                style: Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                            const Placeholder(fallbackHeight: 1000),
+                          ],
+                        ),
+                      ),
+                      WoltModalSheetPage(
+                        stickyActionBar: stickyActionBar,
+                        child: const Column(
+                          children: <Widget>[
+                            FlutterLogo(size: 400),
+                            Placeholder(fallbackHeight: 1000),
+                          ],
+                        ),
+                      ),
+                    ];
+
+                    return pages
+                        .map((WoltModalSheetPage page) => page)
+                        .toList();
                   },
-                  onModalDismissedWithBarrierTap: () =>  Navigator.of(context).pop(),
+                  onModalDismissedWithBarrierTap: () =>
+                      Navigator.of(context).pop(),
                 );
               },
               child: const Text('Show Sheet'),
